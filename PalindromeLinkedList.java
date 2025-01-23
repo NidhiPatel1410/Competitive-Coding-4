c18c7097373283cbfac338a6fc20b319ea023250
Nidhi Patel <patelnidhim14@gmail.com>
2025-01-23 00:23:45 -0500

    Completed

--- PalindromeLinkedList.java
+++ PalindromeLinkedList.java
@@ -12,31 +12,55 @@
 
 class Solution {
     public boolean isPalindrome(ListNode head) {
+        // Base case if head is null or head.next is null meaning only one element, then
+        // it is palindrome
         if (head == null || head.next == null) {
             return true;
         }
+        // To find the mid of LL
+        // Declare slow and fast pointers both starting at head
         ListNode slow = head;
         ListNode fast = head;
+        // Go on until fast not null and fast.next not null (In case of even and odd it
+        // will be different, either fast will be at null or fast will be at end node)
         while (fast != null && fast.next != null) {
+            // Increment slow by 1 and fast by 2
             slow = slow.next;
             fast = fast.next.next;
         }
+        // After finding the mid of LL, i.e slow pointer node now, reverse the second
+        // half of LL, i.e. from mid to end
+        // To reverse, declare 2 pointers, prev which is null and curr which is the mid
+        // of LL(i.e. slow)
         ListNode prev = null;
+        // Go until you reach the end of LL
         while (slow != null) {
+            // Store the slow.next value in tmp
             ListNode tmp = slow.next;
+            // Update slow.next to prev
             slow.next = prev;
+            // Then increment both pointers(prev to slow and slow to slow.next(which was
+            // stored in tmp))
             prev = slow;
             slow = tmp;
         }
+        // Then two pointers, one at head
         ListNode left = head;
+        // Other at the start of reversed LL, which is at prev now
         ListNode right = prev;
+        // Loop for half LL
         while (right != null) {
+            // Check if both value are not equal
             if (left.val != right.val) {
+                // It is not palindrome, return false
                 return false;
             }
+            // Otherwise increment pointers
             left = left.next;
             right = right.next;
         }
+        // If the loop ended without returning false, then it is a palindrome, so return
+        // true
         return true;
 
     }
